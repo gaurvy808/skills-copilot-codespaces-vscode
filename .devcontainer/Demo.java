7dc7a73fd6d812ed9a3265b5c9a781474832c606
gaurvy808 <gaurvy808@gmail.com>
2024-08-21 10:27:50 +0000

    first commit

---
+++ .devcontainer/Demo.java
@@ -0,0 +1,11 @@
+class Addition{
+    public static void main(String [] args){
+    int a=5;
+    int b=20;
+    int result=callMethod(a,b);
+}
+public int result (int a,int b)
+{
+    return a+b;
+}
+}
